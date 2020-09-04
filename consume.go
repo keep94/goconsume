356900d999ab33034ad4587543fd3b1471588e4e
Travis Keep <keep94@gmail.com>
2020-09-04 15:05:53 -0700

    gofmt everything.

--- consume.go
+++ consume.go
@@ -2,33 +2,33 @@
 package goconsume
 
 import (
-  "reflect"
+	"reflect"
 )
 
 const (
-  kCantConsume = "Can't consume"
+	kCantConsume = "Can't consume"
 )
 
 // Consumer consumes values. The values that Consumer consumes must
 // support assignment.
 type Consumer interface {
 
-  // CanConsume returns true if this instance can consume a value
-  CanConsume() bool
+	// CanConsume returns true if this instance can consume a value
+	CanConsume() bool
 
-  // Consume consumes the value that ptr points to. Consume panics if
-  // CanConsume() returns false.
-  Consume(ptr interface{})
+	// Consume consumes the value that ptr points to. Consume panics if
+	// CanConsume() returns false.
+	Consume(ptr interface{})
 }
 
 // ConsumeFinalizer adds a Finalize method to Consumer.
 type ConsumeFinalizer interface {
-  Consumer
+	Consumer
 
-  // Caller calls Finalize after it is done passing values to this consumer.
-  // Once caller calls Finalize(), CanConsume() returns false and Consume()
-  // panics.
-  Finalize()
+	// Caller calls Finalize after it is done passing values to this consumer.
+	// Once caller calls Finalize(), CanConsume() returns false and Consume()
+	// panics.
+	Finalize()
 }
 
 // ConsumerFunc can always consume.
@@ -36,26 +36,26 @@ type ConsumerFunc func(ptr interface{})
 
 // Consume invokes c, this function.
 func (c ConsumerFunc) Consume(ptr interface{}) {
-  c(ptr)
+	c(ptr)
 }
 
 // CanConsume always returns true.
 func (c ConsumerFunc) CanConsume() bool {
-  return true
+	return true
 }
 
 // MustCanConsume panics if c cannot consume
 func MustCanConsume(c Consumer) {
-  if !c.CanConsume() {
-    panic(kCantConsume)
-  }
+	if !c.CanConsume() {
+		panic(kCantConsume)
+	}
 }
 
 // Nil returns a consumer that consumes nothing. Calling CanConsume() on
 // returned consumer returns false, and calling Consume() on returned
 // consumer panics.
 func Nil() Consumer {
-  return nilConsumer{}
+	return nilConsumer{}
 }
 
 // AppendTo returns a Consumer that appends consumed values to the slice
@@ -63,8 +63,8 @@ func Nil() Consumer {
 // slice of values supporting assignment. The CanConsume method of returned
 // consumer always returns true.
 func AppendTo(aValueSlicePointer interface{}) Consumer {
-  aSliceValue := sliceValueFromP(aValueSlicePointer, false)
-  return &appendConsumer{buffer: aSliceValue}
+	aSliceValue := sliceValueFromP(aValueSlicePointer, false)
+	return &appendConsumer{buffer: aSliceValue}
 }
 
 // AppendPtrsTo returns a Consumer that appends consumed values to the slice
@@ -76,10 +76,10 @@ func AppendTo(aValueSlicePointer interface{}) Consumer {
 // pointers to values supporting assignment. The CanConsume method of
 // returned consumer always returns true.
 func AppendPtrsTo(aPointerSlicePointer interface{}) Consumer {
-  aSliceValue := sliceValueFromP(aPointerSlicePointer, true)
-  aSliceType := aSliceValue.Type()
-  allocType := aSliceType.Elem().Elem()
-  return &appendConsumer{buffer: aSliceValue, allocType: allocType}
+	aSliceValue := sliceValueFromP(aPointerSlicePointer, true)
+	aSliceType := aSliceValue.Type()
+	allocType := aSliceType.Elem().Elem()
+	return &appendConsumer{buffer: aSliceValue, allocType: allocType}
 }
 
 // Compose returns the consumers passed to it as a single Consumer. When
@@ -92,11 +92,11 @@ func AppendPtrsTo(aPointerSlicePointer interface{}) Consumer {
 // values being consumed. Wrap any such consumer that modifies the values it
 // consumes with the Copy method or use ComposeWithCopy.
 func Compose(consumers ...Consumer) Consumer {
-  consumerList := make([]Consumer, len(consumers))
-  copy(consumerList, consumers)
-  result := &multiConsumer{consumers: consumerList}
-  result.filterFinished()
-  return result
+	consumerList := make([]Consumer, len(consumers))
+	copy(consumerList, consumers)
+	result := &multiConsumer{consumers: consumerList}
+	result.filterFinished()
+	return result
 }
 
 // Copy returns a consumer that copies the value it consumes using assignment
@@ -104,11 +104,11 @@ func Compose(consumers ...Consumer) Consumer {
 // value being consumed. Callers generally pass nil for it like this:
 // (*TypeBeingConsumed)(nil).
 func Copy(consumer Consumer, valuePtr interface{}) Consumer {
-  spareValuePtr := reflect.New(reflect.TypeOf(valuePtr).Elem())
-  return &consumerWithValue{
-      Consumer: consumer,
-      valuePtr: spareValuePtr.Interface(),
-      value: spareValuePtr.Elem()}
+	spareValuePtr := reflect.New(reflect.TypeOf(valuePtr).Elem())
+	return &consumerWithValue{
+		Consumer: consumer,
+		valuePtr: spareValuePtr.Interface(),
+		value:    spareValuePtr.Elem()}
 }
 
 // ComposeWithCopy is like Compose except that ComposeWithCopy passes a
@@ -117,13 +117,13 @@ func Copy(consumer Consumer, valuePtr interface{}) Consumer {
 // Callers generally pass nil for it like this:
 // (*TypeBeingConsumed)(nil).
 func ComposeWithCopy(consumers []Consumer, valuePtr interface{}) Consumer {
-  consumerList := make([]Consumer, len(consumers))
-  for i := range consumers {
-    consumerList[i] = Copy(consumers[i], valuePtr)
-  }
-  result := &multiConsumer{consumers: consumerList}
-  result.filterFinished()
-  return result
+	consumerList := make([]Consumer, len(consumers))
+	for i := range consumers {
+		consumerList[i] = Copy(consumers[i], valuePtr)
+	}
+	result := &multiConsumer{consumers: consumerList}
+	result.filterFinished()
+	return result
 }
 
 // Slice returns a Consumer that passes the start th value consumed
@@ -136,7 +136,7 @@ func ComposeWithCopy(consumers []Consumer, valuePtr interface{}) Consumer {
 // end values. Note that if end <= start, the underlying consumer will never
 // get any values.
 func Slice(consumer Consumer, start, end int) Consumer {
-  return &sliceConsumer{consumer: consumer, start: start, end: end}
+	return &sliceConsumer{consumer: consumer, start: start, end: end}
 }
 
 // FilterFunc filters values. It returns true if the value should be
@@ -147,16 +147,16 @@ type FilterFunc func(ptr interface{}) bool
 // All returns a FilterFunc that returns true for a given value if and only
 // if all the filters passed to All return true for that same value.
 func All(filters ...FilterFunc) FilterFunc {
-  filterList := make([]FilterFunc, len(filters))
-  copy(filterList, filters)
-  return func(ptr interface{}) bool {
-    for _, filter := range filterList {
-      if !filter(ptr) {
-        return false
-      }
-    }
-    return true
-  }
+	filterList := make([]FilterFunc, len(filters))
+	copy(filterList, filters)
+	return func(ptr interface{}) bool {
+		for _, filter := range filterList {
+			if !filter(ptr) {
+				return false
+			}
+		}
+		return true
+	}
 }
 
 // Filter returns a Consumer that passes only filtered values onto consumer.
@@ -164,9 +164,9 @@ func All(filters ...FilterFunc) FilterFunc {
 // value.  The CanConsume() method of returned Consumer returns true if and
 // only if the CanConsume() method of consumer returns true.
 func Filter(consumer Consumer, filter FilterFunc) Consumer {
-  return &filterConsumer{
-      Consumer: consumer,
-      filter:filter}
+	return &filterConsumer{
+		Consumer: consumer,
+		filter:   filter}
 }
 
 // MapFunc maps values. It applies a transformation to srcPtr and stores the
@@ -186,11 +186,11 @@ type MapFunc func(srcPtr, destPtr interface{}) bool
 // CanConsume() of returned consumer returns true if and only if the
 // CanConsume() method of the consumer parameter returns true.
 func Map(consumer Consumer, mapfunc MapFunc, valuePtr interface{}) Consumer {
-  spareValuePtr := reflect.New(reflect.TypeOf(valuePtr).Elem())
-  return &mapConsumer{
-      Consumer: consumer,
-      mapfunc: mapfunc,
-      valuePtr: spareValuePtr.Interface()}
+	spareValuePtr := reflect.New(reflect.TypeOf(valuePtr).Elem())
+	return &mapConsumer{
+		Consumer: consumer,
+		mapfunc:  mapfunc,
+		valuePtr: spareValuePtr.Interface()}
 }
 
 // Page returns a consumer that does pagination. The items in page fetched
@@ -200,197 +200,197 @@ func Map(consumer Consumer, mapfunc MapFunc, valuePtr interface{}) Consumer {
 // aValueSlicePointer and morePages are undefined until caller calls
 // Finalize() on returned ConsumeFinalizer.
 func Page(
-    zeroBasedPageNo int,
-    itemsPerPage int,
-    aValueSlicePointer interface{},
-    morePages *bool) ConsumeFinalizer {
-  ensureCapacity(aValueSlicePointer, itemsPerPage + 1)
-  truncateTo(aValueSlicePointer, 0)
-  consumer := AppendTo(aValueSlicePointer)
-  consumer = Slice(
-      consumer,
-      zeroBasedPageNo * itemsPerPage,
-      (zeroBasedPageNo + 1) * itemsPerPage + 1)
-  return &pageConsumer{
-      Consumer: consumer,
-      itemsPerPage: itemsPerPage,
-      aValueSlicePointer: aValueSlicePointer,
-      morePages: morePages}
+	zeroBasedPageNo int,
+	itemsPerPage int,
+	aValueSlicePointer interface{},
+	morePages *bool) ConsumeFinalizer {
+	ensureCapacity(aValueSlicePointer, itemsPerPage+1)
+	truncateTo(aValueSlicePointer, 0)
+	consumer := AppendTo(aValueSlicePointer)
+	consumer = Slice(
+		consumer,
+		zeroBasedPageNo*itemsPerPage,
+		(zeroBasedPageNo+1)*itemsPerPage+1)
+	return &pageConsumer{
+		Consumer:           consumer,
+		itemsPerPage:       itemsPerPage,
+		aValueSlicePointer: aValueSlicePointer,
+		morePages:          morePages}
 }
 
 type pageConsumer struct {
-  Consumer
-  itemsPerPage int
-  aValueSlicePointer interface{}
-  morePages *bool
-  finalized bool
+	Consumer
+	itemsPerPage       int
+	aValueSlicePointer interface{}
+	morePages          *bool
+	finalized          bool
 }
 
 func (p *pageConsumer) Finalize() {
-  if p.finalized {
-    return
-  }
-  p.finalized = true
-  p.Consumer = nilConsumer{}
-  if lengthOfSlicePtr(p.aValueSlicePointer) == p.itemsPerPage + 1 {
-    *p.morePages = true
-    truncateTo(p.aValueSlicePointer, p.itemsPerPage)
-  } else {
-    *p.morePages = false
-  }
+	if p.finalized {
+		return
+	}
+	p.finalized = true
+	p.Consumer = nilConsumer{}
+	if lengthOfSlicePtr(p.aValueSlicePointer) == p.itemsPerPage+1 {
+		*p.morePages = true
+		truncateTo(p.aValueSlicePointer, p.itemsPerPage)
+	} else {
+		*p.morePages = false
+	}
 }
 
 func ensureCapacity(aSlicePointer interface{}, capacity int) {
-  value := reflect.ValueOf(aSlicePointer).Elem()
-  if value.Cap() < capacity {
-    typ := value.Type()
-    value.Set(reflect.MakeSlice(typ, 0, capacity))
-  }
+	value := reflect.ValueOf(aSlicePointer).Elem()
+	if value.Cap() < capacity {
+		typ := value.Type()
+		value.Set(reflect.MakeSlice(typ, 0, capacity))
+	}
 }
 
 func truncateTo(aSlicePointer interface{}, newLength int) {
-  value := reflect.ValueOf(aSlicePointer).Elem()
-  value.Set(value.Slice(0, newLength))
+	value := reflect.ValueOf(aSlicePointer).Elem()
+	value.Set(value.Slice(0, newLength))
 }
 
 func lengthOfSlicePtr(aSlicePointer interface{}) int {
-  value := reflect.ValueOf(aSlicePointer).Elem()
-  return value.Len()
+	value := reflect.ValueOf(aSlicePointer).Elem()
+	return value.Len()
 }
 
 type filterConsumer struct {
-  Consumer
-  filter FilterFunc
+	Consumer
+	filter FilterFunc
 }
 
 func (f *filterConsumer) Consume(ptr interface{}) {
-  MustCanConsume(f)
-  if f.filter(ptr) {
-    f.Consumer.Consume(ptr)
-  }
+	MustCanConsume(f)
+	if f.filter(ptr) {
+		f.Consumer.Consume(ptr)
+	}
 }
 
 type mapConsumer struct {
-  Consumer
-  mapfunc MapFunc
-  valuePtr interface{}
+	Consumer
+	mapfunc  MapFunc
+	valuePtr interface{}
 }
 
 func (m *mapConsumer) Consume(ptr interface{}) {
-  MustCanConsume(m)
-  if m.mapfunc(ptr, m.valuePtr) {
-    m.Consumer.Consume(m.valuePtr)
-  }
+	MustCanConsume(m)
+	if m.mapfunc(ptr, m.valuePtr) {
+		m.Consumer.Consume(m.valuePtr)
+	}
 }
 
 type sliceConsumer struct {
-  consumer Consumer
-  start int
-  end int
-  idx int
+	consumer Consumer
+	start    int
+	end      int
+	idx      int
 }
 
 func (s *sliceConsumer) CanConsume() bool {
-  return s.consumer.CanConsume() && s.idx < s.end
+	return s.consumer.CanConsume() && s.idx < s.end
 }
 
 func (s *sliceConsumer) Consume(ptr interface{}) {
-  MustCanConsume(s)
-  if s.idx >= s.start {
-    s.consumer.Consume(ptr)
-  }
-  s.idx++
+	MustCanConsume(s)
+	if s.idx >= s.start {
+		s.consumer.Consume(ptr)
+	}
+	s.idx++
 }
 
 type consumerWithValue struct {
-  Consumer
-  valuePtr interface{}
-  value reflect.Value
+	Consumer
+	valuePtr interface{}
+	value    reflect.Value
 }
 
 func (c *consumerWithValue) Consume(ptr interface{}) {
-  MustCanConsume(c)
-  c.value.Set(reflect.ValueOf(ptr).Elem())
-  c.Consumer.Consume(c.valuePtr)
+	MustCanConsume(c)
+	c.value.Set(reflect.ValueOf(ptr).Elem())
+	c.Consumer.Consume(c.valuePtr)
 }
 
 type multiConsumer struct {
-  consumers []Consumer
+	consumers []Consumer
 }
 
 func (m *multiConsumer) CanConsume() bool {
-  return len(m.consumers) > 0
+	return len(m.consumers) > 0
 }
 
 func (m *multiConsumer) Consume(ptr interface{}) {
-  MustCanConsume(m)
-  for _, consumer := range m.consumers {
-    consumer.Consume(ptr)
-  }
-  m.filterFinished()
+	MustCanConsume(m)
+	for _, consumer := range m.consumers {
+		consumer.Consume(ptr)
+	}
+	m.filterFinished()
 }
 
 func (m *multiConsumer) filterFinished() {
-  idx := 0
-  for i := range m.consumers {
-    if m.consumers[i].CanConsume() {
-      m.consumers[idx] = m.consumers[i]
-      idx++
-    }
-  }
-  for i := idx; i < len(m.consumers); i++ {
-    m.consumers[i] = nil
-  }
-  m.consumers = m.consumers[0:idx]
+	idx := 0
+	for i := range m.consumers {
+		if m.consumers[i].CanConsume() {
+			m.consumers[idx] = m.consumers[i]
+			idx++
+		}
+	}
+	for i := idx; i < len(m.consumers); i++ {
+		m.consumers[i] = nil
+	}
+	m.consumers = m.consumers[0:idx]
 }
 
 type appendConsumer struct {
-  buffer reflect.Value
-  allocType reflect.Type
+	buffer    reflect.Value
+	allocType reflect.Type
 }
 
 func (a *appendConsumer) CanConsume() bool {
-  return true
+	return true
 }
 
 func (a *appendConsumer) Consume(ptr interface{}) {
-  valueToConsume := reflect.ValueOf(ptr).Elem()
-  if a.allocType == nil {
-    a.buffer.Set(reflect.Append(a.buffer, valueToConsume))
-  } else {
-    newPtr := reflect.New(a.allocType)
-    newPtr.Elem().Set(valueToConsume)
-    a.buffer.Set(reflect.Append(a.buffer, newPtr))
-  }
+	valueToConsume := reflect.ValueOf(ptr).Elem()
+	if a.allocType == nil {
+		a.buffer.Set(reflect.Append(a.buffer, valueToConsume))
+	} else {
+		newPtr := reflect.New(a.allocType)
+		newPtr.Elem().Set(valueToConsume)
+		a.buffer.Set(reflect.Append(a.buffer, newPtr))
+	}
 }
 
 func sliceValueFromP(
-    aSlicePointer interface{}, sliceOfPtrs bool) reflect.Value {
-  resultPtr := reflect.ValueOf(aSlicePointer)
-  if resultPtr.Type().Kind() != reflect.Ptr {
-    panic("A pointer to a slice is expected.")
-  }
-  return checkSliceValue(resultPtr.Elem(), sliceOfPtrs)
+	aSlicePointer interface{}, sliceOfPtrs bool) reflect.Value {
+	resultPtr := reflect.ValueOf(aSlicePointer)
+	if resultPtr.Type().Kind() != reflect.Ptr {
+		panic("A pointer to a slice is expected.")
+	}
+	return checkSliceValue(resultPtr.Elem(), sliceOfPtrs)
 }
 
 func checkSliceValue(
-    result reflect.Value, sliceOfPtrs bool) reflect.Value {
-  if result.Kind() != reflect.Slice {
-    panic("a slice is expected.")
-  }
-  if sliceOfPtrs && result.Type().Elem().Kind() != reflect.Ptr {
-    panic("a slice of pointers is expected.")
-  }
-  return result
+	result reflect.Value, sliceOfPtrs bool) reflect.Value {
+	if result.Kind() != reflect.Slice {
+		panic("a slice is expected.")
+	}
+	if sliceOfPtrs && result.Type().Elem().Kind() != reflect.Ptr {
+		panic("a slice of pointers is expected.")
+	}
+	return result
 }
 
 type nilConsumer struct {
 }
 
 func (n nilConsumer) CanConsume() bool {
-  return false
+	return false
 }
 
 func (n nilConsumer) Consume(ptr interface{}) {
-  panic(kCantConsume)
+	panic(kCantConsume)
 }
--- consume_test.go
+++ consume_test.go
@@ -1,216 +1,216 @@
 package goconsume_test
 
 import (
-  "strconv"
-  "testing"
+	"strconv"
+	"testing"
 
-  "github.com/keep94/goconsume"
-  "github.com/stretchr/testify/assert"
+	"github.com/keep94/goconsume"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNil(t *testing.T) {
-  assert := assert.New(t)
-  consumer := goconsume.Nil()
-  assert.False(consumer.CanConsume())
-  assert.Panics(func() { consumer.Consume(new(int)) })
+	assert := assert.New(t)
+	consumer := goconsume.Nil()
+	assert.False(consumer.CanConsume())
+	assert.Panics(func() { consumer.Consume(new(int)) })
 }
 
 func TestPageConsumer(t *testing.T) {
-  assert := assert.New(t)
-  var arr []int
-  var morePages bool
-  pager := goconsume.Page(0, 5, &arr, &morePages)
-  feedInts(t, pager)
-  pager.Finalize()
-  pager.Finalize() // check idempotency of Finalize
-  assert.Equal([]int{0,1,2,3,4}, arr)
-  assert.True(morePages)
-  assert.False(pager.CanConsume())
-  assert.Panics(func() { pager.Consume(new(int)) })
+	assert := assert.New(t)
+	var arr []int
+	var morePages bool
+	pager := goconsume.Page(0, 5, &arr, &morePages)
+	feedInts(t, pager)
+	pager.Finalize()
+	pager.Finalize() // check idempotency of Finalize
+	assert.Equal([]int{0, 1, 2, 3, 4}, arr)
+	assert.True(morePages)
+	assert.False(pager.CanConsume())
+	assert.Panics(func() { pager.Consume(new(int)) })
 
-  pager = goconsume.Page(3, 5, &arr, &morePages)
-  feedInts(t, pager)
-  pager.Finalize()
-  assert.Equal([]int{15,16,17,18,19}, arr)
-  assert.True(morePages)
-  assert.False(pager.CanConsume())
-  assert.Panics(func() { pager.Consume(new(int)) })
+	pager = goconsume.Page(3, 5, &arr, &morePages)
+	feedInts(t, pager)
+	pager.Finalize()
+	assert.Equal([]int{15, 16, 17, 18, 19}, arr)
+	assert.True(morePages)
+	assert.False(pager.CanConsume())
+	assert.Panics(func() { pager.Consume(new(int)) })
 
-  pager = goconsume.Page(2, 5, &arr, &morePages)
-  feedInts(t, goconsume.Slice(pager, 0, 15))
-  pager.Finalize()
-  assert.Equal([]int{10,11,12,13,14}, arr)
-  assert.False(morePages)
-  assert.False(pager.CanConsume())
-  assert.Panics(func() { pager.Consume(new(int)) })
+	pager = goconsume.Page(2, 5, &arr, &morePages)
+	feedInts(t, goconsume.Slice(pager, 0, 15))
+	pager.Finalize()
+	assert.Equal([]int{10, 11, 12, 13, 14}, arr)
+	assert.False(morePages)
+	assert.False(pager.CanConsume())
+	assert.Panics(func() { pager.Consume(new(int)) })
 
-  pager = goconsume.Page(2, 5, &arr, &morePages)
-  feedInts(t, goconsume.Slice(pager, 0, 11))
-  pager.Finalize()
-  assert.Equal([]int{10}, arr)
-  assert.False(morePages)
-  assert.False(pager.CanConsume())
-  assert.Panics(func() { pager.Consume(new(int)) })
+	pager = goconsume.Page(2, 5, &arr, &morePages)
+	feedInts(t, goconsume.Slice(pager, 0, 11))
+	pager.Finalize()
+	assert.Equal([]int{10}, arr)
+	assert.False(morePages)
+	assert.False(pager.CanConsume())
+	assert.Panics(func() { pager.Consume(new(int)) })
 
-  pager = goconsume.Page(2, 5, &arr, &morePages)
-  feedInts(t, goconsume.Slice(pager, 0, 10))
-  pager.Finalize()
-  assert.Equal([]int{}, arr)
-  assert.False(morePages)
-  assert.False(pager.CanConsume())
-  assert.Panics(func() { pager.Consume(new(int)) })
+	pager = goconsume.Page(2, 5, &arr, &morePages)
+	feedInts(t, goconsume.Slice(pager, 0, 10))
+	pager.Finalize()
+	assert.Equal([]int{}, arr)
+	assert.False(morePages)
+	assert.False(pager.CanConsume())
+	assert.Panics(func() { pager.Consume(new(int)) })
 }
 
 func TestCompose(t *testing.T) {
-  assert := assert.New(t)
-  var zeroToFive []int
-  var timesTen []int
-  consumer := goconsume.Compose(
-      goconsume.Nil(),
-      goconsume.Copy(
-          goconsume.Filter(
-              goconsume.Slice(goconsume.AppendTo(&timesTen), 0, 100),
-              func(ptr interface{}) bool {
-                p := ptr.(*int)
-                *p *= 10
-                return true
-              }), (*int)(nil)),
-      goconsume.Slice(goconsume.AppendTo(&zeroToFive), 0, 5),
-  )
-  feedInts(t, consumer)
-  assert.Equal([]int{0, 1, 2, 3, 4}, zeroToFive)
+	assert := assert.New(t)
+	var zeroToFive []int
+	var timesTen []int
+	consumer := goconsume.Compose(
+		goconsume.Nil(),
+		goconsume.Copy(
+			goconsume.Filter(
+				goconsume.Slice(goconsume.AppendTo(&timesTen), 0, 100),
+				func(ptr interface{}) bool {
+					p := ptr.(*int)
+					*p *= 10
+					return true
+				}), (*int)(nil)),
+		goconsume.Slice(goconsume.AppendTo(&zeroToFive), 0, 5),
+	)
+	feedInts(t, consumer)
+	assert.Equal([]int{0, 1, 2, 3, 4}, zeroToFive)
 }
 
 func TestConsumer(t *testing.T) {
-  assert := assert.New(t)
-  var zeroToFive []int
-  var threeToSeven []int
-  var sevensTo28 []int
-  var timesTen []int
-  var oneToThreePtr []*int
-  onePtr := new(int)
-  twoPtr := new(int)
-  *onePtr = 1
-  *twoPtr = 2
-  consumer := goconsume.ComposeWithCopy(
-      []goconsume.Consumer{
-          goconsume.Nil(),
-          goconsume.Filter(
-              goconsume.Slice(goconsume.AppendTo(&timesTen), 0, 100),
-          func(ptr interface{}) bool {
-            p := ptr.(*int)
-            *p *= 10
-            return true
-          }),
-          goconsume.Slice(goconsume.AppendTo(&zeroToFive), 0, 5),
-          goconsume.Slice(goconsume.AppendTo(&threeToSeven), 3, 7),
-          goconsume.Slice(goconsume.AppendPtrsTo(&oneToThreePtr), 1, 3),
-          goconsume.Filter(
-              goconsume.Slice(goconsume.AppendTo(&sevensTo28), 1, 4),
-              func(ptr interface{}) bool {
-                i := *ptr.(*int)
-                return i % 7 == 0
-              })},
-          (*int)(nil))
-  feedInts(t, consumer)
-  assert.Equal([]int{0, 1, 2, 3, 4}, zeroToFive)
-  assert.Equal([]int{3, 4, 5, 6}, threeToSeven)
-  assert.Equal([]*int{onePtr, twoPtr}, oneToThreePtr)
-  assert.Equal([]int{7, 14, 21}, sevensTo28)
-  assert.Equal(10, timesTen[1])
-  assert.Equal(20, timesTen[2])
+	assert := assert.New(t)
+	var zeroToFive []int
+	var threeToSeven []int
+	var sevensTo28 []int
+	var timesTen []int
+	var oneToThreePtr []*int
+	onePtr := new(int)
+	twoPtr := new(int)
+	*onePtr = 1
+	*twoPtr = 2
+	consumer := goconsume.ComposeWithCopy(
+		[]goconsume.Consumer{
+			goconsume.Nil(),
+			goconsume.Filter(
+				goconsume.Slice(goconsume.AppendTo(&timesTen), 0, 100),
+				func(ptr interface{}) bool {
+					p := ptr.(*int)
+					*p *= 10
+					return true
+				}),
+			goconsume.Slice(goconsume.AppendTo(&zeroToFive), 0, 5),
+			goconsume.Slice(goconsume.AppendTo(&threeToSeven), 3, 7),
+			goconsume.Slice(goconsume.AppendPtrsTo(&oneToThreePtr), 1, 3),
+			goconsume.Filter(
+				goconsume.Slice(goconsume.AppendTo(&sevensTo28), 1, 4),
+				func(ptr interface{}) bool {
+					i := *ptr.(*int)
+					return i%7 == 0
+				})},
+		(*int)(nil))
+	feedInts(t, consumer)
+	assert.Equal([]int{0, 1, 2, 3, 4}, zeroToFive)
+	assert.Equal([]int{3, 4, 5, 6}, threeToSeven)
+	assert.Equal([]*int{onePtr, twoPtr}, oneToThreePtr)
+	assert.Equal([]int{7, 14, 21}, sevensTo28)
+	assert.Equal(10, timesTen[1])
+	assert.Equal(20, timesTen[2])
 }
 
 func TestSlice(t *testing.T) {
-  assert := assert.New(t)
-  var zeroToFive []int
-  feedInts(t, goconsume.Slice(goconsume.AppendTo(&zeroToFive), 0, 5))
-  assert.Equal([]int{0, 1, 2, 3, 4}, zeroToFive)
+	assert := assert.New(t)
+	var zeroToFive []int
+	feedInts(t, goconsume.Slice(goconsume.AppendTo(&zeroToFive), 0, 5))
+	assert.Equal([]int{0, 1, 2, 3, 4}, zeroToFive)
 }
 
 func TestFilter(t *testing.T) {
-  assert := assert.New(t)
-  var sevensTo28 []int
-  feedInts(t, goconsume.Filter(
-      goconsume.Slice(goconsume.AppendTo(&sevensTo28), 1, 4),
-      func(ptr interface{}) bool {
-        i := *ptr.(*int)
-        return i % 7 == 0
-      }))
-  assert.Equal([]int{7, 14, 21}, sevensTo28)
+	assert := assert.New(t)
+	var sevensTo28 []int
+	feedInts(t, goconsume.Filter(
+		goconsume.Slice(goconsume.AppendTo(&sevensTo28), 1, 4),
+		func(ptr interface{}) bool {
+			i := *ptr.(*int)
+			return i%7 == 0
+		}))
+	assert.Equal([]int{7, 14, 21}, sevensTo28)
 }
 
 func TestMap(t *testing.T) {
-  assert := assert.New(t)
-  var zeroTo10By2 []string
-  feedInts(t, goconsume.Map(
-      goconsume.Slice(goconsume.AppendTo(&zeroTo10By2), 0, 6),
-      func (srcPtr, destPtr interface{}) bool {
-        src := *srcPtr.(*int)
-        if src % 2 == 1 {
-          return false
-        }
-        *destPtr.(*string) = strconv.Itoa(src)
-        return true
-      },
-      (*string)(nil)))
-  assert.Equal([]string{"0", "2", "4", "6", "8", "10"}, zeroTo10By2)
+	assert := assert.New(t)
+	var zeroTo10By2 []string
+	feedInts(t, goconsume.Map(
+		goconsume.Slice(goconsume.AppendTo(&zeroTo10By2), 0, 6),
+		func(srcPtr, destPtr interface{}) bool {
+			src := *srcPtr.(*int)
+			if src%2 == 1 {
+				return false
+			}
+			*destPtr.(*string) = strconv.Itoa(src)
+			return true
+		},
+		(*string)(nil)))
+	assert.Equal([]string{"0", "2", "4", "6", "8", "10"}, zeroTo10By2)
 }
 
 func TestAllImmutability(t *testing.T) {
-  assert := assert.New(t)
-  multipleOfTwo := func(ptr interface{}) bool {
-    return (*ptr.(*int)) % 2 == 0
-  }
-  multipleOfThree := func(ptr interface{}) bool {
-    return (*ptr.(*int)) % 3 == 0
-  }
-  filters := []goconsume.FilterFunc{multipleOfTwo, multipleOfThree}
-  allFilter := goconsume.All(filters...)
-  x := 6
-  assert.True(allFilter(&x))
-  x = 10
-  assert.False(allFilter(&x))
-  filters[1] = func(ptr interface{}) bool {
-    return (*ptr.(*int)) % 5 == 0
-  }
+	assert := assert.New(t)
+	multipleOfTwo := func(ptr interface{}) bool {
+		return (*ptr.(*int))%2 == 0
+	}
+	multipleOfThree := func(ptr interface{}) bool {
+		return (*ptr.(*int))%3 == 0
+	}
+	filters := []goconsume.FilterFunc{multipleOfTwo, multipleOfThree}
+	allFilter := goconsume.All(filters...)
+	x := 6
+	assert.True(allFilter(&x))
+	x = 10
+	assert.False(allFilter(&x))
+	filters[1] = func(ptr interface{}) bool {
+		return (*ptr.(*int))%5 == 0
+	}
 
-  // Even though we mutated the filters array allFilter should still behave
-  // the same as before.
-  x = 6
-  assert.True(allFilter(&x))
-  x = 10
-  assert.False(allFilter(&x))
+	// Even though we mutated the filters array allFilter should still behave
+	// the same as before.
+	x = 6
+	assert.True(allFilter(&x))
+	x = 10
+	assert.False(allFilter(&x))
 }
 
 func TestAll(t *testing.T) {
-  assert := assert.New(t)
-  multipleOfTwo := func(ptr interface{}) bool {
-    return (*ptr.(*int)) % 2 == 0
-  }
-  multipleOfThree := func(ptr interface{}) bool {
-    return (*ptr.(*int)) % 3 == 0
-  }
-  filter := goconsume.All(multipleOfTwo, multipleOfThree)
-  x := 1
-  assert.False(filter(&x))
-  x = 2
-  assert.False(filter(&x))
-  x = 3
-  assert.False(filter(&x))
-  x = 6
-  assert.True(filter(&x))
+	assert := assert.New(t)
+	multipleOfTwo := func(ptr interface{}) bool {
+		return (*ptr.(*int))%2 == 0
+	}
+	multipleOfThree := func(ptr interface{}) bool {
+		return (*ptr.(*int))%3 == 0
+	}
+	filter := goconsume.All(multipleOfTwo, multipleOfThree)
+	x := 1
+	assert.False(filter(&x))
+	x = 2
+	assert.False(filter(&x))
+	x = 3
+	assert.False(filter(&x))
+	x = 6
+	assert.True(filter(&x))
 }
-    
+
 func feedInts(t *testing.T, consumer goconsume.Consumer) {
-  assert := assert.New(t)
-  idx := 0
-  for consumer.CanConsume() {
-    nidx := idx
-    consumer.Consume(&nidx)
-    idx++
-  }
-  assert.Panics(func() {
-    consumer.Consume(&idx)
-  })
+	assert := assert.New(t)
+	idx := 0
+	for consumer.CanConsume() {
+		nidx := idx
+		consumer.Consume(&nidx)
+		idx++
+	}
+	assert.Panics(func() {
+		consumer.Consume(&idx)
+	})
 }

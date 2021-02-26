57e470b0f1420b26268ab0af13a5046614ea1bb0
Travis Keep <keep94@gmail.com>
2021-02-26 03:53:12 -0800

    Use assert.Same when possible in internal_consume_test.go

--- internal_consume_test.go
+++ internal_consume_test.go
@@ -9,22 +9,23 @@ import (
 func TestNestedMapFilter(t *testing.T) {
 	assert := assert.New(t)
 	var result []string
-	c := MapFilter(AppendTo(&result), appendStr("d"), appendStr("e"))
+	a := AppendTo(&result)
+	c := MapFilter(a, appendStr("d"), appendStr("e"))
 	c = MapFilter(c, appendStr("a"), appendStr("b"), appendStr("c"))
 	if c.CanConsume() {
 		str := ""
 		c.Consume(&str)
 	}
 	assert.Equal([]string{"abcde"}, result)
-	mpc := c.(*mapFilterConsumer)
-	assert.IsType((*appendConsumer)(nil), mpc.Consumer)
+	mfc := c.(*mapFilterConsumer)
+	assert.Same(a, mfc.Consumer)
 }
 
 func TestMapFilterWithNil(t *testing.T) {
 	assert := assert.New(t)
 	var ints []int
 	c := AppendTo(&ints)
-	assert.Equal(c, MapFilter(c, NewApplier()))
+	assert.Same(c, MapFilter(c, NewApplier()))
 }
 
 func TestComposeZero(t *testing.T) {
@@ -36,7 +37,7 @@ func TestComposeOne(t *testing.T) {
 	assert := assert.New(t)
 	var ints []int
 	c := AppendTo(&ints)
-	assert.Equal(c, Compose(c))
+	assert.Same(c, Compose(c))
 }
 
 func appendStr(s string) func(src, dest *string) bool {
